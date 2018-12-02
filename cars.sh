68bcd2e07ce8bb2b108fce8a8388b733615c925f
Alexander Ono <ono@chapman.edu>
2018-12-02 00:02:02 -0800

    fixed colon usage and added case statements

--- cars.sh
+++ cars.sh
@@ -1,36 +1,41 @@
 #! /bin/bash
-#numbers.sh
+#cars.sh
 #Alexander Ono
 
 #cars.sh
 
+
 clear
 while true; do
-	echo "What would you like to do? :"
+	x=":"
+	echo "What would you like to do?:"
 	echo "[1] Add a car"
 	echo "[2] List cars"
 	echo "[3] Quit the program"
 
 	read input
-
-	if [ $input == 1 ] ; then
-		echo "What is the year?"
-		read year
-		echo "What is the make?"
-		read make
-		echo "What is the model?"
-		read model
-		echo $year $make $model >> My_old_cars.txt	
-		clear
-	elif [ $input == 2 ] ; then
-		sort My_old_cars.txt
-
-	elif [ $input == 3 ] ; then
-		clear
-		echo "Goodbye!"
-		exit 113
-
-	else
-		echo "Invalid Input..."
-	fi
+		
+	case $input in
+		1)
+			echo "What is the year?"
+			read year
+			echo "What is the make?"
+			read make
+			echo "What is the model?"
+			read model
+			combination="$year$x$make$x$model"
+			echo $combination >> My_old_cars.txt	
+			clear
+			;;
+		2)
+			sort My_old_cars.txt | tr : " "
+			;;
+		3)
+			clear
+			echo "Goodbye!"
+			exit 113
+			;;
+		*)
+			echo "Invalid Input..."
+	esac
 done	
